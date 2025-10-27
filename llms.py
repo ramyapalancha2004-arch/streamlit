d0249a6de46dcb1c84d4554b09d52763e02ecdd7
RAMYA PALANCHA <ramyapalancha2004@gmail.com>
2025-10-27 18:56:00 +0530

    Add files via upload

---
+++ llms.py
@@ -0,0 +1,49 @@
+import streamlit as st
+st.header("LLM'S")
+
+
+st.subheader("Introduction")
+st.write("""LLMs (Large Language Models)are advanced AI models trained to understand, generate, and interact with human language.  
+They are built using deep learning techniques and massive amounts of text data to perform tasks like **chatting, summarizing, translating, and coding.""")
+
+
+st.subheader("Key Features")
+st.write("""1. Natural Language Understanding-Can comprehend text context and meaning.  
+2. Text Generation – Can write essays, code, emails, and more.  
+3. Context Awareness – Keeps track of conversation or text context.  
+4. Knowledge Across Domains– Trained on diverse topics: science, tech, culture, and code.  
+5. Adaptable– Can be fine-tuned for specific tasks like customer support or medical analysis.""")  
+
+
+
+st.subheader("How LLMs Work")
+st.write("""1. Training on Huge Text Data– Books, articles, websites, code, and more.  
+2. Transformer Architecture – Uses self-attention to understand relationships between words.  
+3. Prediction – Predicts the next word or token in a sequence.  
+4. Fine-tuning– Can be trained further for specialized tasks.""")
+
+
+st.subheader("Examples of LLMs")
+st.write("""Model:GPT-4 / GPT-5,Gemini,Claude,LLaMA
+        Creator :OpenAI,Google DeepMind,Anthropic,Meta
+        Example Use:Chatbots, coding, content generation,Search, AI assistants,Document analysis, writing support,Research and AI experiment""")
+
+
+st.subheader("Applications of LLMs")
+st.write("""1. Chatbots & Virtual Assistants – ChatGPT, Alexa, Siri  
+2. Content Creation – Writing articles, essays, resumes  
+3. Code Generation – Copilot, AI coding assistants  
+4. Data Analysis & Summarization – Extract insights from large documents  
+5. Translation – Convert text between languages  
+6. Education & Tutoring– Answer questions, explain concepts.""") 
+
+
+st.subheader("Limitations")
+st.write("""1. May generate incorrect information** (hallucination).  
+2. No real understanding or emotions.  
+3. Knowledge is limited to training data unless connected to live sources.""")  
+
+
+
+st.markdown("[Wikipedia - Large Language Model](https://en.wikipedia.org/wiki/Large_language_model) ") 
+st.markdown("[OpenAI GPT Models](https://openai.com/research) ")
